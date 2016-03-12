70528c1a43a18ac38cb70d2ec56307109999763f
Tree Casiano <tree.pdx@gmail.com>
2016-03-12 09:58:23 -0800

    Group layer styles.

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,6 +1,4 @@
-/**
- * Created by Tree on 3/12/16.
- */
+// layer styles
 function nbhoodLayerStyle() {
   return {
     fillColor: '#3CE646',
@@ -8,6 +6,30 @@ function nbhoodLayerStyle() {
     weight: 1,
     opacity: 0.8,
     color: '#3CE646'
+  };
+}
+
+// TODO: remove hard-coding of the year
+function timelineLayerStyle(data) {
+  var year = parseInt(data.properties['start']);//.get('year'));
+  // console.log(data.properties.issuedate);
+  var color;
+  if (typeof data.properties.issuedate !== 'undefined') {
+    // console.log('not undefined');
+    dataType = "permits";
+    color = getPermitColor(2015);//getPermitColor(year);
+  } else {
+    dataType = "demolitions";
+    color = getDemolitionsColor(2015);//getDemolitionsColor(year);
+  }
+
+  return {
+    radius: 8,
+    fillColor: color,//getPermitColor(year, dataType),//'#FF5500',
+    color: '#000',
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.7
   };
 }
 
@@ -73,28 +95,5 @@ function getColor(d) {
                     '#EBFCEC';
 }
 
-// TODO: remove hard-coding of the year
 
-function timelineLayerStyle(data) {
-      var year = parseInt(data.properties['start']);//.get('year'));
-      // console.log(data.properties.issuedate);
-      var color;
-      if (typeof data.properties.issuedate !== 'undefined') {
-        // console.log('not undefined');
-        dataType = "permits";
-        color = getPermitColor(2015);//getPermitColor(year);
-      } else {
-        dataType = "demolitions";
-        color = getDemolitionsColor(2015);//getDemolitionsColor(year);
-      }
-
-      return {
-        radius: 8,
-        fillColor: color,//getPermitColor(year, dataType),//'#FF5500',
-        color: '#000',
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.7
-      };
-    }
 
