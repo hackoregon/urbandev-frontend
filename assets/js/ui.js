5aa61eb50afc9d3ff8e006c81c107aae346ec7ce
Tree Casiano <tree.pdx@gmail.com>
2016-03-12 09:08:25 -0800

    Initial commit.

---
+++ assets/js/ui.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Tree on 3/12/16.
+ */
+  function nbhoodLayerStyle() {
+    return {
+      fillColor: '#3CE646',
+      fillOpacity: 0.8,
+      weight: 1,
+      opacity: 0.8,
+      color: '#3CE646'
+    };
+  }
