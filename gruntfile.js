b619c28e7f832caf73373410d6606ab8e7259bc7
Rob Vella <me@robvella.com>
2016-04-07 20:08:45 -0700

    Add grunt-connect, livereload, and grunt-open. Remove production.js from being versioned, as it's generated by concat in Gruntfile.js.

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,8 @@
+LIVERELOAD_PORT = 35728;
+
 module.exports = function(grunt) {
   grunt.initConfig({
+    app: '.',
     pkg: grunt.file.readJSON('package.json'),
     sass: {
       dist: {
@@ -20,11 +23,45 @@ module.exports = function(grunt) {
     watch: {
       css: {
         files: 'assets/scss/**.scss',
-        tasks: 'sass'
+        tasks: 'sass',
+        options: {
+          livereload: LIVERELOAD_PORT
+        }
       },
       scripts: {
         files: ['assets/js/app.js', 'assets/js/api.js'],
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
         tasks: 'concat'
+      },
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: [
+          "**/*.html"
+        ]
+      }
+    },
+    open: {
+      server: {
+        url: "http://localhost:<%= connect.server.options.port %>"
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          livereload: LIVERELOAD_PORT,
+          base: {
+            path: '.',
+            options: {
+              index: 'index.html',
+              maxAge: 300000
+            }
+          }
+        }
       }
     }
   });
@@ -32,4 +69,5 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.registerTask('default', ['concat']);
+  grunt.registerTask('serve', ['concat', 'connect:server', 'open', 'watch']);
 }
